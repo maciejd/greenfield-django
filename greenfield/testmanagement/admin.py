522ea5154fdcf3a8fdd89471fe42f1cb9246bd45
Maciej Dobrowolski <dobrowolski6@gmail.com>
2016-11-21 20:44:03 +0100

    customized admin form, added nested views

--- greenfield/testmanagement/admin.py
+++ greenfield/testmanagement/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import TestCase, TestRun, TestSuite, TestExecution
 
-# Register your models here.
+
+class CaseInline(admin.StackedInline):
+    model = TestCase
+
+class SuiteAdmin(admin.ModelAdmin):
+    inlines = [CaseInline]
+
+class ExecutionInline(admin.StackedInline):
+    readonly_fields = ['updated_at']
+    model = TestExecution
+
+class RunAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at']
+    inlines = [ExecutionInline]
+
+admin.site.register(TestSuite, SuiteAdmin)
+admin.site.register(TestRun, RunAdmin)
+
