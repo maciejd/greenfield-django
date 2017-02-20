82ae3eaf19feb8412e644f33d07d8011f26aed51
Maciej Dobrowolski <maciej.dobrowolski@atos.net>
2017-02-20 16:10:39 +0100

    added login_required decorator to views and urls

--- greenfield/testmanagement/urls.py
+++ greenfield/testmanagement/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 app_name = 'greenfield'
 urlpatterns = [
-    url(r'^suite/$', views.SuiteListView.as_view(), name='suites'),
-    url(r'^suite/(?P<pk>[0-9]+)/$', views.SuiteDetailView.as_view(), name='suite'),
+    url(r'^suite/$', login_required(views.SuiteListView.as_view()), name='suites'),
+    url(r'^suite/(?P<pk>[0-9]+)/$', login_required(views.SuiteDetailView.as_view()), name='suite'),
     url(r'^suite/(?P<suite_id>[0-9]+)/add$', views.add_case, name='add_case'),
     url(r'^suite/(?P<suite_id>[0-9]+)/delete/(?P<case_id>[0-9]+)/$', views.delete_case, name='delete_case'),
     url(r'^suite/add$', views.add_suite, name='add_suite'),
@@ -16,7 +17,7 @@ urlpatterns = [
     url(r'^run/(?P<run_id>[0-9]+)/', views.show_run, name='run'),
     url(r'^run/add$', views.add_run, name='add_run'),
     url(r'^run/delete/(?P<run_id>[0-9]+)/&$', views.delete_run, name='delete_run'),
-    url(r'case/(?P<pk>[0-9]+)/$', views.CaseDetailView.as_view(), name='case'),
+    url(r'case/(?P<pk>[0-9]+)/$', login_required(views.CaseDetailView.as_view()), name='case'),
     url(r'^login/$', auth_views.login,  {'template_name': 'testmanagement/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'greenfield:login'}, name='logout'),
     ]
--- greenfield/testmanagement/views.py
+++ greenfield/testmanagement/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 
 from .models import TestRun,TestSuite,TestCase,TestExecution
 
@@ -13,28 +14,32 @@ class SuiteDetailView(generic.DetailView):
     model = TestSuite
     template_name = 'testmanagement/suite_detail.html'
 
+@login_required
 def add_suite(request):
     TestSuite(title=request.POST['title']).save()
     return HttpResponseRedirect(reverse('greenfield:suites'))
-
+@login_required
 def delete_suite(request, suite_id):
     get_object_or_404(TestSuite, pk=suite_id).delete()
     return HttpResponseRedirect(reverse('greenfield:suites'))
-
+@login_required
 def add_case(request, suite_id):
     ts = get_object_or_404(TestSuite, pk=suite_id)
     TestCase(title=request.POST['title'], suite=ts).save()
     return HttpResponseRedirect(reverse('greenfield:suite', kwargs={'pk':suite_id}))
-   
+
+@login_required
 def delete_case(request, suite_id, case_id):
     get_object_or_404(TestCase, pk=case_id).delete()
     return HttpResponseRedirect(reverse('greenfield:suite', kwargs={'pk':suite_id}))
 
+@login_required
 def show_runs(request):
     runs = TestRun.objects.all()
     suites = TestSuite.objects.exclude(testcase__isnull=True)
     return render(request, 'testmanagement/run_list.html', {'runs': runs, 'suites': suites})
 
+@login_required
 def add_run(request):
     ts = get_object_or_404(TestSuite, pk=request.POST['suite_id'])
     tr = TestRun(title=request.POST['title'], suite=ts)
@@ -42,15 +47,18 @@ def add_run(request):
     for tc in ts.testcase_set.all():
         TestExecution(case=tc, run=tr, status=0).save()
     return HttpResponseRedirect(reverse('greenfield:runs'))
-  
+
+@login_required
 def delete_run(request, run_id):
     get_object_or_404(TestRun, pk=run_id).delete()
     return HttpResponseRedirect(reverse('greenfield:runs'))
-    
+
+@login_required
 def show_run(request, run_id):
     tr = get_object_or_404(TestRun, pk=run_id)
     return render(request, 'testmanagement/run_detail.html', {'run': tr, 'STATUSES':TestExecution.STATUSES})
 
+@login_required
 def update_result(request, run_id, execution_id):
     e = get_object_or_404(TestExecution, pk=execution_id)
     e.status=request.POST['status']
